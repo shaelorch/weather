8f1ad74aea832c793c93b4e5aad6444bcc09ac88
Shae Lorch <slorch@my.bcit.ca>
2023-01-17 11:01:13 -0800

    ini

---
+++ .history/pages/index_20230117104017.js
@@ -0,0 +1,54 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+import axios from 'axios'
+import { useState } from 'react';
+
+const inter = Inter({ subsets: ['latin'] })
+
+
+
+export default function Home() {
+  const [location, setLocation] = useState('');
+  const [data, setData] = useState({});
+  const [weather, setWeather] = useState();
+  const [errorMessage, setErrorMessage] = useState();
+
+  var apiKey = "5bb65e5dcdd88d96c09dd61a44557240";
+  var lang = "fr";
+  var units = "metric";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&appid=${apiKey}&lang=${lang}`
+
+  const searchLocation = (event) => {
+    if(event.key === "Enter") {
+      axios.get(url)
+      .then((response) => {
+        console.clear();
+        setData(response.data);
+        console.log(response.data);
+        setWeather(response.data.weather);
+        setErrorMessage("");
+      }).catch(err => {
+        console.log(err);
+        setErrorMessage("Please enter another location");
+        setData({});
+        setWeather()
+      })
+      setLocation('')
+  }}
+
+  return <main className={styles.main}>
+    <input 
+    value={location}
+    onChange={event => setLocation(event.target.value)}
+    placeholder="Enter Location"
+    onKeyDown={searchLocation}
+    type="text"
+    />
+    
+  </main>
+    
+     
+    
+}
